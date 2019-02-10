bfa16457b79d44d50c0c6172eb67e9c25bc1985f
Haniel Hernández <harenjaegger@gmail.com>
2019-02-09 20:45:35 -0600

    Added animation and menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// import {sal} from "sal.js";
 var technologies=[
         {name:"HTML5",image:"assets/images/html_logo.png"},
         {name:"CSS3",image:"assets/images/css3_logo.png"},
@@ -13,42 +14,47 @@ var projectss=[
     {name:"Invemu",description:"Loremp iasdasd",link:""}
 ];
 var open=false;
+var menu = document.getElementById('responsive-menu');
+var lines= document.querySelectorAll('.btn-line');
+
+    
     for(i=0;i<technologies.length;i++){
         var technology=technologies[i];
         var div = document.createElement('div');
-        div.className = 'col-3 technologies';
-        div.innerHTML = "<img src='"+technology.image+"'  style='width:128px; height:128px; margin:0px auto; display:block;'  alt=''> <p style='text-align: center;'>"+technology.name+"</p>"
+        div.className = 'col-2 technologies';
+        div.innerHTML = "<img src='"+technology.image+"'  style='width:100%; margin:0px auto; display:block;'  alt=''> <p style='text-align: center;'>"+technology.name+"</p>"
         document.getElementById("technologies").appendChild(div);
         
     }
-    document.getElementById('menu-button').addEventListener('click',function(){
-      
-        open= !open;
-        openMenu();
-    });
-    function openMenu(){
-        var menu = document.getElementsByClassName('hidden-menu')[0];
-        if(open==true){
-            document.getElementById('menu-button').innerHTML="<i class='fas fa-times' style='font-size:35px'></i>";
-            menu.setAttribute('style',"display:block;")
+    document.getElementById('menu-btn').addEventListener('click',function(){
+        console.log(open);
+        if(open==false){
+            menu.setAttribute('class','menu-open');
+            lines[0].classList.add('line-left');
+            lines[1].classList.add('line-erased');
+            lines[2].classList.add('line-right');
+            open=true;
         }else{
-            document.getElementById('menu-button').innerHTML="<i class='fas fa-bars' style='font-size:35px'></i>";
-            menu.setAttribute('style',"display:none;")
-        }
-      
-    }
+            menu.setAttribute('class',"menu-close");
+            lines[0].classList.remove('line-left');
+            lines[1].classList.remove('line-erased');
+            lines[2].classList.remove('line-right');
+            open=false;
+        }   
+        console.log(open);
+    });
+
+
+
     document.addEventListener('scroll',function(){
         var normalMenu=document.getElementsByClassName('link');
        // normalMenu.st"color: rgb(0,0,0);")
         
-       console.log(window.scro)
-       if(Window.scrollY==1507){
+       console.log(window.scrollY);
+       if(Window.scrollY>1507){
            console.log("cambiar");
-        for(i=0;i< normalMenu.length; i++){
-            normalMenu[i].setAttribute('style',"color: rgb(0,0,0);");
-        }
+            console.log(normalMenu);
        }
-        
     })
     document.get
     
